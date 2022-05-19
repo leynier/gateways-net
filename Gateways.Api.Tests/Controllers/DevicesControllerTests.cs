4992f2054bf46df639936326fafa23d9363585f6
Leynier Gutiérrez González <leynier41@gmail.com>
2022-05-19 01:09:05 -0400

    tests: implement code base for unit tests of devices controller

---
+++ Gateways.Api.Tests/Controllers/DevicesControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Gateways.Api.Controllers;
+using Gateways.Api.Tests;
+using Gateways.Business.Contracts.Services;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Devices.Api.Tests.Controllers;
+
+public class DevicesControllerTests
+{
+    private readonly IConfiguration config;
+    private readonly Mock<IDeviceService> deviceService;
+    private readonly IMapper mapper;
+
+    public DevicesControllerTests()
+    {
+        config = new ConfigurationBuilder().Build();
+        deviceService = new Mock<IDeviceService>();
+        mapper = AutoMapperFactory.CreateMapper();
+    }
+
+    private DevicesController Controller => new(deviceService.Object, mapper, config);
+}
