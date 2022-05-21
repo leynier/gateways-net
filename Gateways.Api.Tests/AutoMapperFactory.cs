1db49c890ed257674a2dc451bbfc188a5c998c64
Leynier Gutiérrez González <leynier41@gmail.com>
2022-05-21 18:56:19 -0400

    perf(tests): auto load profiles in the factory class of mapper

--- Gateways.Api.Tests/AutoMapperFactory.cs
+++ Gateways.Api.Tests/AutoMapperFactory.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Gateways.Api.MapperProfiles;
 
 namespace Gateways.Api.Tests;
 
@@ -7,10 +6,20 @@ public static class AutoMapperFactory
 {
     public static IMapper CreateMapper()
     {
+        var profiles = AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .Where(x => x.Namespace == "Gateways.Api.MapperProfiles")
+            .Where(x => !x.ContainsGenericParameters)
+            .Where(x => x.IsClass)
+            .Where(x => !x.IsAbstract)
+            .Where(x => x.IsAssignableTo(typeof(Profile)))
+            .ToList();
         return new MapperConfiguration(config =>
         {
-            config.AddProfile(new DeviceProfile());
-            config.AddProfile(new GatewayProfile());
+            foreach (var profile in profiles)
+                config.AddProfile(profile);
         }).CreateMapper();
     }
 }
