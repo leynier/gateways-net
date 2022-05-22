8a14bf6fb07e19f6f2cb9de4646507ad32bbb7aa
Leynier Gutiérrez González <leynier41@gmail.com>
2022-05-22 01:25:21 -0400

    tests: implement http client factory

---
+++ Gateways.Api.IntegrationTests/ClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Gateways.Api.IntegrationTests;
+
+public static class ClientFactory
+{
+    public static HttpClient? CreateClient() => new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder => { })
+            .CreateClient();
+}
